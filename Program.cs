577fbdbdd12db33bd4e0ee0383d831ea2b915133
BjornBEs <bjorn@r66.dk>
2022-10-21 18:40:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_in_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
